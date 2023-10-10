ec52fc66b58e7e882a6913a8aa76e92857dc42a1
diegobaena89 <diegobf.cello@gmail.com>
2023-10-09 21:03:22 -0300

    feat: add stitches config

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      textPrimary: "#c5c5c5",
+    },
+  },
+});
